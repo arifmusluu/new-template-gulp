4f070905eb7d3792fb2c6a5c903017509977a741
arifmusluu <arifmuslu96@gmail.com>
2021-01-29 12:54:18 +0300

    aa

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-const gulp = require("gulp"),
-    connect = require('gulp-connect'),
-    sass = require('gulp-sass');
+var gulp = require("gulp");
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var cleanCSS = require("gulp-clean-css");
 
 gulp.task('connect', function() {
     connect.server({
@@ -15,12 +17,28 @@ gulp.task('scss-concat', function() {
         .pipe(gulp.dest('assets/src/css'));
 });
 
+
+gulp.task('js-concat', function() {
+    return gulp.src('assets/src/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/src/js'));
+});
+
+gulp.task("cssmin", function() {
+    return gulp.src("assets/src/css/*.css")
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("assets/dist/css"));
+});
+
 gulp.task("default", ["cssmin", "js-concat", "scss-concat"], function() {
 
+    gulp.watch("assets/src/css/main.css", ["cssmin"]);
+    gulp.watch("assets/src/js/**/*.js", ["js-concat"]);
     gulp.watch("assets/scss/**/*.scss", ["scss-concat"]);
 
 });
 
 
 
-gulp.task('run', ['connect', 'scss-concat']);
+gulp.task('run', ['connect', 'scss-concat', 'js-concat', 'cssmin']);
